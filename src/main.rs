03deaabadfc797931e6bf4ace10566e8757ae269
Trace <trace@DESKTOP-8BNPO0U.home>
2024-04-08 13:43:30 -0600

    Added imgsrc folder, and a saveapps.json for storing applications added to program, user input is working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Please enter your pin:");
+    io::stdin().read_line(&mut input).expect("Failed to read pin");
+    let pin: i32 = input.trim().parse().expect("Invalid input");
+    println!("Your pin: {}", pin);
 }
